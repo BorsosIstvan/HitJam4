59105413fdd64951b39c2ce7315d329026b47feb
BorsosIstvan <bistvanus@gmail.com>
2026-07-03 18:45:14 +0200

    party speel

--- speel.php
+++ speel.php
@@ -23,7 +23,7 @@ try {
 // 3. ITUNES API INTEGRATIE
 $schone_artiest = str_replace('&', ' ', $song['artist']);
 $zoekterm = urlencode($schone_artiest . " " . $song['title']);
-$api_url = "https://apple.com" . $zoekterm . "&limit=1&entity=song";
+$api_url = "https://itunes.apple.com/search?term=" . $zoekterm . "&limit=1&entity=song";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $api_url);
